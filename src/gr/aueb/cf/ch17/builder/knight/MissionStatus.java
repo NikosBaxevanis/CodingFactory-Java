42cc9863fe6beb83f9248480d25ceec50ede12f9
NikosBaxevanis <baxemb@gmail.com>
2025-01-21 20:32:16 +0200

    14/01/2025

--- src/gr/aueb/cf/ch17/builder/knight/MissionStatus.java
+++ src/gr/aueb/cf/ch17/builder/knight/MissionStatus.java
@@ -6,3 +6,6 @@ public enum MissionStatus {
     STARTED,
     NOT_STARTED
 }
+
+//Τα enums είναι σαν τις κλάσεις. Είναι τύπος δεδομένων. Περιέχει μόνο σταθερές ώστε να οργανώνουμε related finals μέσα σε μία δόμη.
+// Π.χ αν είχαμε δύο σταθερές ENABLED, DISABLED, αντί να έχουμε final String ENABLED = "ENABLED"; και final String DISABLED = "DISABLED", μπορούμε να έχουμε ένα enum {ENABLED, DISABLED}
