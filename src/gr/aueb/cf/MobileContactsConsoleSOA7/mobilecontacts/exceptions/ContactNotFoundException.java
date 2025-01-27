581ba7a321efff8c2b5fe9deca5e4571e3c5539e
NikosBaxevanis <baxemb@gmail.com>
2025-01-27 21:00:58 +0200

    14/01/2025

---
+++ src/gr/aueb/cf/MobileContactsConsoleSOA7/mobilecontacts/exceptions/ContactNotFoundException.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.MobileContactsConsoleSOA7.mobilecontacts.exceptions;
+
+public class ContactNotFoundException  extends  Exception{
+    public ContactNotFoundException (String message) {
+        super(message);
+    }
+}
---
+++ src/gr/aueb/cf/MobileContactsConsoleSOA7/mobilecontacts/exceptions/PhoneNumberAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.MobileContactsConsoleSOA7.mobilecontacts.exceptions;
+
+public class PhoneNumberAlreadyExistsException  extends  Exception{
+
+    public PhoneNumberAlreadyExistsException(String message) {
+        super(message);
+    }
+}
