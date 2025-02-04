ee78c93d4eec3f0db2b31e96728b4e6494822ad7
NikosBaxevanis <baxemb@gmail.com>
2025-02-04 18:52:55 +0200

    04/02/2025 Complexity

--- src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
+++ src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
@@ -1,20 +1,21 @@
 package gr.aueb.cf.ch19.doubly_linked_list;
 
 public class DoublyLinkedList <T> {
-    private Node <T>  head ;
-    private Node <T> tail;
+    private Node<T> head;
+    private Node<T> tail;
 
-    public DoublyLinkedList () {
+    public DoublyLinkedList() {
 
     }
 
 
     /**
      * Time Complexity 0(1)
+     *
      * @param t the value to be inserted
      */
     public void insertFirst(T t) {
-        Node <T> tmp = new Node<>();
+        Node<T> tmp = new Node<>();
         tmp.setItem(t);
         tmp.setNext(head);
         tmp.setPrev(null);
@@ -27,21 +28,95 @@ public class DoublyLinkedList <T> {
 
     /**
      * Time-Complexity 0(1)
+     *
      * @param t
      */
-    public void insertLast (T t) {
+    public void insertLast(T t) {
         if (head == null) {
             insertFirst(t);
             return;
         }
 
-        Node <T> tmp = new Node<>();
+        Node<T> tmp = new Node<>();
         tmp.setItem(t);
         tmp.setNext(null);
         tmp.setPrev(tail);
 
         tail.setNext(tmp);
-        tail=tmp;
+        tail = tmp;
 
     }
-}
+
+
+    /**
+     * Time-Complexity 0(1)
+     * @return the removed node.
+     */
+
+    public Node<T> removeFirst() {
+        Node<T> nodeToReturn;
+
+        if (isEmpty()) return null;
+
+        nodeToReturn = head;
+        if (head.getNext() == null) tail = null;
+        head = head.getNext();
+        head.setPrev(null);
+        return nodeToReturn;
+    }
+
+
+    /**
+     * Time-Complexity 0(1)
+     * @return the removed node
+     */
+    public Node <T> removeLast() {
+        Node <T> nodeToReturn;
+        if (isEmpty() || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        nodeToReturn = tail;
+        tail = tail.getPrev();
+        tail.setNext(null);
+        return nodeToReturn;
+    }
+
+
+    public Node<T> get(T t) {
+        Node<T> nodeToReturn = null;
+
+        for (Node <T> n = head; n!=null; n=n.getNext()) {
+            if (n.getItem().equals(t)) {
+                nodeToReturn = n;
+                break;
+            }
+        }
+        return nodeToReturn;
+    }
+
+
+    /**
+     * Time complexity 0(n)
+     */
+    public void traverse() {
+        for (Node <T> n = head; n!=null; n=n.getNext()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    /**
+     * Time complexity 0(n)
+     */
+    public void traverseReverse() {
+        for (Node <T> n = head; n!=null; n=n.getPrev()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+
+    public boolean isEmpty () {
+            return head == null;
+        }
+    }
+
