3fc971b32dde6128b7c7b0e79929c9b25abf43df
NikosBaxevanis <baxemb@gmail.com>
2025-03-18 19:12:51 +0200

    18/03/2025

---
+++ src/gr/aueb/cf/SchoolSwing7Pro_Eclipse/LandingPage.java
@@ -0,0 +1,23 @@
+//package gr.aueb.cf.SchoolSwing7Pro_Eclipse;
+//
+//import javax.swing.*;
+//import javax.swing.border.EmptyBorder;
+//
+//public class LandingPage extends JFrame {
+//
+//    private static final long serialVersionUID = 1L;
+//    private JPanel contentPane;
+//
+//    public LandingPage() {
+//        setTitle("Ποιότητα στην Εκπαιδευση");
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setBounds(100,100,450,300);
+//        contentPane = new JPanel();
+//        contentPane.setBorder(new EmptyBorder(5,5,5,5));
+//
+//        setContentPane(contentPane);
+//        contentPane.setLayout(null);
+//
+//
+//    }
+//}
---
+++ src/gr/aueb/cf/SchoolSwing7Pro_Eclipse/Main.java
@@ -0,0 +1,25 @@
+//package gr.aueb.cf.SchoolSwing7Pro_Eclipse;
+//
+//import java.awt.*;
+//
+//public class Main {
+//private final static LandingPage  LANDING_PAGE = new LandingPage();
+//    /**
+//     * Launch the application
+//     * @param args
+//     */
+//    public static void main(String[] args) {
+//        EventQueue. invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                   LANDING_PAGE.setVisibile(true);
+//                   LANDING_PAGE.setLocationRelativeTo(null);
+//
+//                }catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+//}
